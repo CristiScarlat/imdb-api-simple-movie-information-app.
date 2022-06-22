10b701ed743e020bdb8526d79ab38702d1a5d487
CristiScarlat <78.cristi@gmail.com>
2022-06-22 17:38:29 +0300

    remove env

--- src/components/watchlist/watchlist.tsx
+++ src/components/watchlist/watchlist.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { StoreContext } from '../../context/store';
 import { Link } from 'react-router-dom';
 import { ArrowIcon, TrashIcon } from '../icons/icons';
